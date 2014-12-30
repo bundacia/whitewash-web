df722325713047e6414095a53dc0e273edff6ae2
Trevor Little <trevor.little@livingsocial.com>
2014-12-30 16:55:59 -0500

    adding redirect to form.html

--- src/whitewash_web/core/handler.clj
+++ src/whitewash_web/core/handler.clj
@@ -1,15 +1,14 @@
 (ns whitewash-web.core.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :refer [redirect]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [whitewash.core :refer [wash unwash]]))  
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/unwash" [text]
-       (unwash text))
-  (GET "/wash" [text]
-       (wash text))
+  (GET "/" [] (redirect "form.html"))
+  (GET "/unwash" [text] (unwash text))
+  (GET "/wash"   [text] (wash   text))
   (route/not-found "Not Found"))
 
 (def app

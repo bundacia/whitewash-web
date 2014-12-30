e5241c9fca43dc892133a960be00da6dd64bd1b3
Trevor Little <trevor.little@livingsocial.com>
2014-12-30 17:07:20 -0500

    using whitewash jar

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [whitewash "0.1.0-SNAPSHOT"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler whitewash-web.core.handler/app}
